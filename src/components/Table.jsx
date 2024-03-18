cf9007367a7a4e45ee2ac8c8c0a7ab29e6fb802e
Lauren Wolfangel <laurenwolfangel@gmail.com>
2024-03-18 17:25:37 -0500

    Orbit Report Update 3/18

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,30 @@
+//import satData from "./satData";
+//import Buttons from "./Buttons";
+//import App from "../App";
+
+
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+     {sat.map((data) => {
+        return (
+          <tr key={data.id}>
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>{data.operational ? "true" : "false"}</td>
+          </tr>
+        )
+        })}
      </tbody>
    </table>
   );
